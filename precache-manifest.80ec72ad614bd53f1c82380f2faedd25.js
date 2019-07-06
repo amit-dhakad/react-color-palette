9cc5597ccd3b62ce072c43761844d23690e5f587
amit dhakad <amit0502dhakad@gmail.com>
2019-07-06 23:08:03 +0530

    Updates

---
+++ precache-manifest.80ec72ad614bd53f1c82380f2faedd25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "923028c315b61dc4c6e5c6e5cc7a3979",
+    "url": "/amit-dhakad/react-color-palette/index.html"
+  },
+  {
+    "revision": "1ea7e803e7e2c6fe911e",
+    "url": "/amit-dhakad/react-color-palette/static/css/main.3a202737.chunk.css"
+  },
+  {
+    "revision": "cd84f01c97fff8be6d85",
+    "url": "/amit-dhakad/react-color-palette/static/js/2.e49f06cb.chunk.js"
+  },
+  {
+    "revision": "1ea7e803e7e2c6fe911e",
+    "url": "/amit-dhakad/react-color-palette/static/js/main.da904c2b.chunk.js"
+  },
+  {
+    "revision": "f92fbe6a06c831364119",
+    "url": "/amit-dhakad/react-color-palette/static/js/runtime~main.3ac7d6c9.js"
+  }
+]);
